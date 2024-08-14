7843fba5d444953ea48cb75931466518c7689f45
SashaPotemkin <alex.potjomkin@gmail.com>
2024-08-14 15:11:20 +0300

    AnimationSpec

--- app/src/main/java/ru/potemkin/composeanimations/ui/screen/Test.kt
+++ app/src/main/java/ru/potemkin/composeanimations/ui/screen/Test.kt
@@ -1,9 +1,17 @@
 package ru.potemkin.composeanimations.ui.screen
 
 import androidx.compose.animation.animateColorAsState
+import androidx.compose.animation.core.LinearEasing
+import androidx.compose.animation.core.RepeatMode
+import androidx.compose.animation.core.Spring
 import androidx.compose.animation.core.animateDpAsState
+import androidx.compose.animation.core.animateFloat
 import androidx.compose.animation.core.animateFloatAsState
 import androidx.compose.animation.core.animateIntAsState
+import androidx.compose.animation.core.infiniteRepeatable
+import androidx.compose.animation.core.rememberInfiniteTransition
+import androidx.compose.animation.core.spring
+import androidx.compose.animation.core.tween
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.*
@@ -17,6 +25,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.rotate
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
@@ -36,7 +45,11 @@ fun Test() {
             mutableStateOf(true)
         }
 
-        val size by animateDpAsState(targetValue = if (isIncreased) 200.dp else 100.dp)
+        val size by animateDpAsState(
+            targetValue = if (isIncreased) 200.dp else 100.dp,
+            animationSpec = spring(dampingRatio = Spring.DampingRatioHighBouncy,
+                stiffness = Spring.StiffnessHigh)
+        )
 
         Button(
             modifier = Modifier.fillMaxWidth(),
@@ -55,7 +68,10 @@ fun Test() {
             mutableStateOf(true)
         }
 
-        val radius by animateIntAsState(targetValue = if (isRect) 4 else 50)
+        val radius by animateIntAsState(
+            targetValue = if (isRect) 4 else 50,
+            animationSpec = tween(durationMillis = 3000, delayMillis = 1000)
+        )
 
         Button(
             modifier = Modifier.fillMaxWidth(),
@@ -74,7 +90,21 @@ fun Test() {
             mutableStateOf(false)
         }
 
-        val borderWidth by animateDpAsState(targetValue = if (isSelected) 4.dp else 0.dp)
+        val infiniteTransition = rememberInfiniteTransition()
+
+        val borderWidth by infiniteTransition.animateFloat(
+            initialValue = 4f,
+            targetValue = 0f,
+            animationSpec = infiniteRepeatable(
+                animation = tween(durationMillis = 3000),
+                repeatMode = RepeatMode.Reverse
+            ))
+//        val borderWidth by animateDpAsState(
+//            targetValue = if (isSelected) 4.dp else 0.dp,
+//            animationSpec = infiniteRepeatable(
+//                animation = tween(durationMillis = 3000)
+//            )
+//        )
 
         Button(
             modifier = Modifier.fillMaxWidth(),
@@ -86,14 +116,26 @@ fun Test() {
         }
         AnimatedContainer(
             text = "Border",
-            borderWidth = borderWidth
+            borderWidth = borderWidth.dp
         )
 
+        val rotation by infiniteTransition.animateFloat(
+            initialValue = 0f,
+            targetValue = 360f,
+            animationSpec = infiniteRepeatable(
+                animation = tween(durationMillis = 3000, easing = LinearEasing),
+
+            ))
         var isBlue by remember {
             mutableStateOf(true)
         }
 
-        val backgroundColor by animateColorAsState(targetValue = if (isBlue) Color.Blue else Color.Magenta)
+        val backgroundColor by animateColorAsState(
+            targetValue = if (isBlue) Color.Blue else Color.Magenta,
+            animationSpec = infiniteRepeatable(
+                animation = tween(durationMillis = 3000),
+            )
+        )
 
         Button(
             modifier = Modifier.fillMaxWidth(),
@@ -105,7 +147,8 @@ fun Test() {
         }
         AnimatedContainer(
             text = "Color",
-            backgroundColor =  backgroundColor
+            backgroundColor =  backgroundColor,
+            rotate = rotation
         )
 
         var isTransparent by remember {
@@ -136,10 +179,12 @@ private fun AnimatedContainer(
     radiusPercent: Int = 4,
     borderWidth: Dp = 0.dp,
     backgroundColor: Color = Color.Blue,
-    alpha: Float = 1f
+    alpha: Float = 1f,
+    rotate:Float = 0f,
 ) {
     Box(
         modifier = Modifier
+            .rotate(rotate)
             .alpha(alpha)
             .clip(RoundedCornerShape(radiusPercent))
             .border(width = borderWidth, color = Color.Black)
